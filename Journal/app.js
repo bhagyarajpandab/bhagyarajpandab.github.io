1cef9aa250e44ee293e2debeb5bc4248277a2fe2
Bhagyaraj Pandab <bhagyaraj.p@highradius.com>
2021-08-06 10:35:32 +0530

    Adding Journal Project to site

---
+++ Journal/app.js
@@ -0,0 +1,69 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const homeStartingContent = "This is a blog page type of journal and you can document your daily life in it";
+const aboutContent = "Nothing Much This website was created to simulate daily journals in a digital format hope you like it.";
+const contactContent = "Hi this is Bhagyraj the developer of this website. Chechout my github in footer";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+let posts = [];
+
+app.get("/", function(req, res){
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts
+    });
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
+
+});
+
+app.get("/posts/:postName", function(req, res){
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach(function(post){
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    }
+  });
+
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
